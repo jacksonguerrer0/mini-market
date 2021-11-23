a3c75147f7260bf5df4df97ac45dbc0f1674c7a4
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-11-23 11:55:40 -0500

    Config initial

---
+++ src/container/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import '../style/style.scss'
+const App = () => {
+  return (
+    <div>
+      <h1 style={{color:'red'}}>hello ok</h1>
+    </div>
+  )
+}
+
+export default App
