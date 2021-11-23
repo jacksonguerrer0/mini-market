26fa6a47e3a51512a327bf208c147cccbf0179b7
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-11-23 14:39:00 -0500

    config store redux

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-// import '../style/style.scss'
+
 const App = () => {
   return (
     <div>
-      <h1 style={{color:'red'}}>hello ok</h1>
+      <h1 >hello ok</h1>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import App from './container/App';
+import './style/style.scss'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <>
-    <h1>Hola we</h1>
-  </>,
+    <Provider store={store} >
+    <App />
+    </Provider>,
   document.getElementById('root')
 );
