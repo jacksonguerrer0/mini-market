380cc9406629e8ba2d6caeb15620fd7b0121299a
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-11-25 08:41:12 -0500

    add product cart

--- src/redux/productsDucks.js
+++ src/redux/productsDucks.js
@@ -15,7 +15,11 @@ const productsDucks = (state= initialState, action) =>{
       ...state,
       products: action.payload
     };
-  
+    case types.ADD_PRODUCT_CART:
+      return{
+        ...state,
+        cart: action.payload
+      }
     default:
       return state;
   }
@@ -29,4 +33,29 @@ export default productsDucks
 export const saveProducts = (products) => ({
   type: types.GET_PRODUCTS,
   payload: products
+})
+
+
+export const addProductCart = (id) => (dispatch, selector) => {
+  const {products} = selector(state => state.products)
+  const product = products.products.find(product => product.id === id)
+  const itemCart = products.cart.find(productCart => productCart.id === id)
+
+
+  dispatch(addCart(
+    itemCart 
+    ? products.cart.map(ele =>{ 
+      return ele.id === id 
+      ?{
+        ...ele, quantity: ele.quantity + 1
+      }
+      : ele
+    })
+    : [...products.cart, {...product, quantity: 1}]
+  ))
+
+}
+const addCart = (product) => ({
+  type: types.ADD_PRODUCT_CART,
+  payload: product
 })
