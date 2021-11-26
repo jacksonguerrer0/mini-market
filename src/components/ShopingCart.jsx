bf5fb4d795350c153a81acad6bdfaf1fc057a70e
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-11-26 07:40:35 -0500

    pasarela wompi

--- src/components/ShopingCart.jsx
+++ src/components/ShopingCart.jsx
@@ -6,6 +6,24 @@ import './shopingCart.scss'
 
 const ShopingCart = () => {
   const { cart } = useSelector(state => state.products)
+
+  const handlePay = (e) => {
+    e.preventDefault()
+    const reference = new Date().getTime()
+    const total = getTotalCart(cart)
+    let checkout = new window.WidgetCheckout({
+      currency: 'COP',
+      amountInCents: total * 18200 ,
+      reference: `jack_${reference}`,
+      publicKey: 'pub_test_AQOb9XRmok39xbAWMNGl59hTROmT2vSH',
+      redirectUrl: 'https://docs.wompi.co/docs/en/metodos-de-pago',
+    })
+    checkout.open(function ( result ) {
+      var transaction = result.transaction
+      console.log('Transaction ID: ', transaction.id)
+      console.log('Transaction object: ', transaction)
+    })
+  }
   return (
     <aside>
       {
@@ -23,10 +41,12 @@ const ShopingCart = () => {
             ))
           : <p><small>Press a product</small></p>
         }
-
+      <form onSubmit={handlePay}>
+      <button>PAGAR</button>
+      </form>
       </section>
       {
-        cart.length > 0 &&  <div className='total-cart'><p><b>Total</b>:</p><h3>{`$${getTotalCart(cart)}`}.00</h3></div>
+        cart.length > 0 &&  <div className='total-cart'><p><b>Total</b>:</p><h3>{`$${getTotalCart(cart)}`}.00 MXN</h3></div>
 
       }
     </aside>
